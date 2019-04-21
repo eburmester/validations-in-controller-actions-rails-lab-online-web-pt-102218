1dfca0cdd1f50b37d454e903edd8e5b50398a497
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 02:07:08 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
     
-    if !@author.is_valid? 
+    if !@author.valid? 
       render :new 
     else 
       @author.save
